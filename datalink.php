78e0d8272137ab8ba25fd59dde93a416a7afc493
Carissa Jansen <carissa@dhcp-18-111-116-30.dyn.mit.edu>
2011-01-16 01:12:11 -0500

    new datalink file

--- datalink.php
+++ datalink.php
@@ -1,32 +1,9 @@
 <?php
-/*************** PHP LOGIN SCRIPT V 2.3*********************
-(c) Balakrishnan 2010. All Rights Reserved
-
-Usage: This script can be used FREE of charge for any commercial or personal projects. Enjoy!
-
-Limitations:
-- This script cannot be sold.
-- This script should have copyright notice intact. Dont remove it please...
-- This script may not be provided for download except from its original site.
-
-For further usage, please contact me.
-
-/******************** MAIN SETTINGS - PHP LOGIN SCRIPT V2.1 **********************
-Please complete wherever marked xxxxxxxxx
-
-/************* MYSQL DATABASE SETTINGS *****************
-1. Specify Database name in $dbname
-2. MySQL host (localhost or remotehost)
-3. MySQL user name with ALL previleges assigned.
-4. MySQL password
-
-Note: If you use cpanel, the name will be like account_database
-*************************************************************/
 
 define ("DB_HOST", "sql.mit.edu"); // set database host
 define ("DB_USER", "rrt"); // set database user
 define ("DB_PASS", "det66mef"); // set database password
-define ("DB_NAME", "rrt+studentusers"); // set database name
+define ("DB_NAME", "rrt+studentusers.users"); // set database name
 
 $link = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die("Couldn't make connection.");
 $db = mysql_select_db(DB_NAME, $link) or die("Couldn't select database");
