347021d3d60aaf8ef930d1c9e46e37546a7809d5
Rishi Tirumala <rishter@gmail.com>
2011-01-20 14:08:40 -0500

    ;

--- seematches.php
+++ seematches.php
@@ -9,7 +9,8 @@ include 'datalink.php';
 <body>
 <?php
 $result = mysql_query("SELECT `Company`,`City`,`State`,`GPA`,`Professional Field` FROM employer_users") or die (mysql_error()); 
-echo "Here are your matches!"
+echo "Here are your matches!";
+//echo "Your gpa is ".$_SESSION['user_gpa'];
 while($row = mysql_fetch_array($result))
   {
 	if($_SESSION['user_gpa'] >= $row['GPA']){
