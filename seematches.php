b3e730fffaff3d6b1e9f29fe036c2303adb11f00
Rishi Tirumala <rishter@gmail.com>
2011-01-19 22:23:03 -0500

    see cookies

---
+++ seematches.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Your matches! Get On Top</title>
+</head>
+<body>
+<?php
+echo $_COOKIE["user_id"];
+echo $_COOKIE["user_key"];
+echo $_COOKIE["user_name"];
+
+
+
+>
+</body>
