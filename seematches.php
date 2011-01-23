a41ea198c62b1d1956e6908dc46654d9af8ed84a
Rishi Tirumala <rishter@gmail.com>
2011-01-23 11:23:07 -0500

    s

--- seematches.php
+++ seematches.php
@@ -9,13 +9,23 @@ page_protect();
 <body>
 <?php
 echo "Here are your matches!<br />";
-echo "Your gpa is ".$_SESSION['user_gpa'];
+
+echo "<table border='1'>
+<tr>
+<th>Company Name</th>
+<th>City</th>
+<th>State</th>
+</tr>";
+
 $result = mysql_query("SELECT `Company`,`City`,`State`,`GPA`,`Professional Field` FROM employer_users") or die (mysql_error()); 
 while($row = mysql_fetch_array($result))
   {
 	if($_SESSION['user_gpa'] >= $row['GPA'] && $_SESSION['user_field'] == $row['Professional Field']){
-		echo $row['Company'] . " in " . $row['City'] . ", " . $row['State'];
-		echo "<br />";
+		echo "<tr>";
+  		echo "<td>" . $_SESSION['Company'] . "</td>";
+  		echo "<td>" . $_SESSION['City'] . "</td>";
+  		echo "<td>" . $_SESSION['State'] . "</td>";
+  		echo "</tr>";
 	}
   }
 ?>
