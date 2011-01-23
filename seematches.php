5d6e9ebd397ccdff1d822687b47dc54c954da9c0
Rishi Tirumala <rishter@gmail.com>
2011-01-23 10:24:36 -0500

    ajax

--- seematches.php
+++ seematches.php
@@ -1,6 +1,29 @@
+
 <?php
 include 'datalink.php';
 page_protect();
+$q=$_GET["q"];
+
+$result = mysql_query("SELECT `Company`,`City`,`State`,`GPA`,`Professional Field` FROM employer_users") or die (mysql_error()); 
+
+echo "<table border='1'>
+<tr>
+<th>Company Name</th>
+<th>City</th>
+<th>State</th>
+</tr>";
+
+while($row = mysql_fetch_array($result))
+  {
+	if($_SESSION['user_gpa'] >= $row['GPA'] && $_SESSION['user_field'] == $row['Professional Field']){
+		echo "<tr>";
+  		echo "<td>" . $row['Company'] . "</td>";
+  		echo "<td>" . $row['City'] . "</td>";
+  		echo "<td>" . $row['State'] . "</td>";
+  		echo "</tr>";
+  }
+echo "</table>";
+
 ?>
 
 <html>
@@ -53,29 +76,5 @@ while($row = mysql_fetch_array($result))
 		echo "<br />";
 	}
   }*/
-?>
-<?php
-$q=$_GET["q"];
-
-$result = mysql_query("SELECT `Company`,`City`,`State`,`GPA`,`Professional Field` FROM employer_users") or die (mysql_error()); 
-
-echo "<table border='1'>
-<tr>
-<th>Company Name</th>
-<th>City</th>
-<th>State</th>
-</tr>";
-
-while($row = mysql_fetch_array($result))
-  {
-	if($_SESSION['user_gpa'] >= $row['GPA'] && $_SESSION['user_field'] == $row['Professional Field']){
-		echo "<tr>";
-  		echo "<td>" . $row['Company'] . "</td>";
-  		echo "<td>" . $row['City'] . "</td>";
-  		echo "<td>" . $row['State'] . "</td>";
-  		echo "</tr>";
-  }
-echo "</table>";
-
 ?>
 </body>
